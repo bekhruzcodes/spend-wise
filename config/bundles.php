e69e56a9b9ff31e3f933f70493775c4e066539ae
Ubuntu <ubuntu@ip-172-31-39-33.eu-north-1.compute.internal>
2025-03-23 12:07:35 +0000

    AWS changes commited

--- config/bundles.php
+++ config/bundles.php
@@ -6,5 +6,6 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+  //  Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
